7731fcf7a5a4817145d4d468aa13bbe6bf543038
004matteos <mattsoho17@gmail.com>
2024-05-12 01:11:20 +0300

    tests

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
