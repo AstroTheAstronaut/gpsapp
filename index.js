81789923c16dad7f4e45648852cb2b7950b5dc5a
004matteos <mattsoho17@gmail.com>
2024-05-12 01:13:59 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.sendFile('/index.html');
+  res.sendFile('index.html');
 });
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
